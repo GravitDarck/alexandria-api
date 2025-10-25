e730a9ec8441038ce137cfd1ed38dc0e61544b34
Pedro de Lucca <pedrogdelucca@gmail.com>
2025-10-25 02:00:42 -0300

    Initial commit

---
+++ src/test/java/br/com/alexandria/alexandria_api/AlexandriaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alexandria.alexandria_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlexandriaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
